4ed0619cc90332925467fe1c2c828dace478e3b8
cdrainxv <cdrainxv@users.noreply.github.com>
2017-11-29 21:54:12 -0700

    update eslint rules and npm script

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,6 +24,7 @@ module.exports = {
         singleQuote: true,
         trailingComma: 'none'
       }
-    ]
+    ],
+    eqeqeq: 'error'
   }
 }
--- src/api/index.js
+++ src/api/index.js
@@ -3,9 +3,4 @@ const getCommon = require('./get_common')
 const getScientific = require('./get_scientific')
 const getRandomName = require('./get_random_name')
 
-module.exports = {
-  getAll,
-  getCommon,
-  getScientific,
-  getRandomName
-}
+module.exports = { getAll, getCommon, getScientific, getRandomName }
