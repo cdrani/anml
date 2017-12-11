2c1525efa0ecbf74bd2b9dd0a433821acc0ce3b4
cdrainxv <cdrainxv@users.noreply.github.com>
2017-12-10 22:23:40 -0700

    add babel-polyfill for Object.entries()

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -3,7 +3,7 @@ import { join } from 'path'
 const include = join(__dirname, 'src')
 
 export default {
-  entry: './src/index',
+  entry: ['babel-polyfill', './src/index'],
   output: {
     path: join(__dirname, 'dist'),
     libraryTarget: 'umd',
